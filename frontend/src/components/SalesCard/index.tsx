9d0dc31c8da876e72abe276c7e38697fc45b7590
alex <als01bm@gmail.com>
2022-09-10 18:14:00 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -17,11 +17,15 @@ function SalesCard(){
     const [sales, setSales] = useState<sale[]>([]);
 
     useEffect(() => {
-      axios.get(`${BASE_URL}/sales`)
+
+      const dMin = minDate.toISOString().slice(0, 10);
+      const dMax = maxDate.toISOString().slice(0, 10);
+
+      axios.get(`${BASE_URL}/sales?minDate=${dMin}&maxDate=${dMax}`)
         .then(response => {
           setSales(response.data.content);
         });
-    }, []);
+    }, [minDate, maxDate]);
 
     return(
         <div className="dsmeta-card">
